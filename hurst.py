e923e02d1b5222e8700ed0a7c880ce4b4b80e321
fndjjx <thunderocean@163.com>
2016-06-23 09:09:35 +0800

    clean code

--- hurst.py
+++ hurst.py
@@ -1,45 +1,13 @@
 
 from numpy.fft import fft
 from numpy.random import randn
-from numpy import zeros, floor, log10, log, mean, array, sqrt, vstack, cumsum, \
-				  ones, log2, std
+from numpy import zeros, floor, log10, log, mean, array, sqrt, vstack, cumsum, ones, log2, std
 from numpy.linalg import svd, lstsq
 import time
 
 ######################## Functions contributed by Xin Liu #################
 
 def hurst(X):
-	""" Compute the Hurst exponent of X. If the output H=0.5,the behavior
-	of the time-series is similar to random walk. If H<0.5, the time-series
-	cover less "distance" than a random walk, vice verse. 
-
-	Parameters
-	----------
-
-	X
-
-		list    
-		
-		a time series
-
-	Returns
-	-------
-	H
-        
-		float    
-
-		Hurst exponent
-
-	Examples
-	--------
-
-	>>> import pyeeg
-	>>> from numpy.random import randn
-	>>> a = randn(4096)
-	>>> pyeeg.hurst(a)
-	>>> 0.5057444
-	
-	"""
 	
 	N = len(X)
 	T = array([float(i) for i in xrange(1,N+1)])
